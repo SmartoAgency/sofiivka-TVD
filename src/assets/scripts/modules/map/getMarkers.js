a8d0cfe7b602b48a5e25d7393964faed4b37f016
Dron2019 <dron_2019@ukr.net>
2025-04-11 00:38:35 +0300

    add google markers

--- src/assets/scripts/modules/map/getMarkers.js
+++ src/assets/scripts/modules/map/getMarkers.js
@@ -1,76 +1,31 @@
-import markersFromPrevSite from "./markersFromPrevSite";
-
-const baseFolder = window.location.href.match(/localhost/) 
+const baseFolder = window.location.href.match(/localhost|verstka/) 
 ? './assets/images/markers/'
-: '/wp-content/themes/forest-home/assets/images/markers/';
+: '/wp-content/themes/3d/assets/images/markers/';
 
 const markersAdresses = {
-    main: `${baseFolder}main.svg`,
-    shop: `${baseFolder}shop.svg`,
-    education: `${baseFolder}education.svg`,
-    cafe: `${baseFolder}cafe.svg`,
-    medicine: `${baseFolder}medicine.svg`,
-    sport: `${baseFolder}sport.svg`,
-    petrol: `${baseFolder}petrol.svg`,
-    bank: `${baseFolder}bank.svg`,
-    post: `${baseFolder}post.svg`,
-    park: `${baseFolder}park.svg`,
-    transport: `${baseFolder}transport.svg`,
-  };
+  main: `${baseFolder}main.svg`,
+  shop: `${baseFolder}shop.svg`,
+  education: `${baseFolder}education.svg`,
+  cafe: `${baseFolder}cafe.svg`,
+  medicine: `${baseFolder}medicine.svg`,
+  sport: `${baseFolder}sport.svg`,
+  petrol: `${baseFolder}petrol.svg`,
+  bank: `${baseFolder}bank.svg`,
+  post: `${baseFolder}post.svg`,
+  park: `${baseFolder}park.svg`,
+  transport: `${baseFolder}transport.svg`,
+};
 
-const markerPopupStyle = `
-style="
-background: #ffffff;
-color:#000000;
-font-weight: bold;
-padding:5px 10px;
-font-size: 16px;
-line-height: 120%;"
-`;
 
 export async function fetchMarkersData(google) {
-
-    
     const buildLogoSize = new google.maps.Size(125, 55);
     const sendData = new FormData();
     sendData.append('action', 'infrastructure');
-    const url = window.location.href.match(/localhost/)
-      ? 'https://central-park-wp.smarto.com.ua/wp-admin/admin-ajax.php'
-      : '/wp-admin/admin-ajax.php'
-    // let markersData = window.location.href.match(/localhost|smarto/) ? Promise.resolve(mockData()) : await fetch(url, {
-    //   method: 'POST',
-    //   body: sendData,
-    // });
-    let markersData = Promise.resolve(mockData());
-    // markersData = window.location.href.match(/localhost|smarto/) ? mockData() : await markersData.json();
-    markersData = mockData();
-    if (!markersData) {
-      console.warn('Wrong data recieved');
-      return;
-    };
 
-    let formatedMarkersDataForMap = markersData.reduce((acc, el) => {
-      if (!el.list) return acc;
-      el.list.forEach(marker => {
-        acc.push({
-          content: `<div ${markerPopupStyle}>${marker.name}</div>`,
-          position: { 
-            lat: marker.coordinations.latitude, 
-            lng: marker.coordinations.elevation 
-          },
-          type: el.code,
-          id: marker.id,
-          zIndex: 2,
-          icon: { url: markersAdresses[el.code], scaledSize: buildLogoSize }
-        });
-      });
-      return acc;
-    }, []);
+    let formatedMarkersDataForMap = [];
+    const markers = await getMarkersFromJson();
 
-
-    console.log(formatedMarkersDataForMap);
-
-    markersFromPrevSite().forEach(marker => {      
+    markers.forEach(marker => {      
         formatedMarkersDataForMap.push({
             content: marker.description,
             position: { 
@@ -87,26 +42,12 @@ export async function fetchMarkersData(google) {
     return formatedMarkersDataForMap;
 }
 
-
-
-function mockData() {
-    return [
-        // {
-        //     "name": "RAMS City Haliç",
-        //     "code": "ramscity",
-        //     "list": [
-        //         {
-        //             "name": "RAMS City Haliç",
-        //             "id": "12312312",
-        //             "coordinations": {
-        //                 "latitude": "41.0334469",
-        //                 "elevation": "28.9212694"
-        //             }
-        //         }
-        //     ]
-        // },
-      
-    ]
+async function getMarkersFromJson() {
+    const url = window.location.href.match(/localhost|verstka/) ? '/static/google-markers.json' : '/wp-content/themes/3d/assets/static/google-markers.json';
+    const response = await fetch(url, {
+        method: 'GET',
+    });
+    const data = await response.json();
+    return data;
 }
-
 
--- src/assets/scripts/modules/map/map.js
+++ src/assets/scripts/modules/map/map.js
@@ -1,51 +1,61 @@
 import { fetchMarkersData } from "./getMarkers";
 import mapStyle from "./map-style";
 
-
-
 export default function googleMap() {
   global.initMap = initMap
 }
 
-async function func() {
+const MAP_PARAMS = {
+  zoom: 16,
+  scrollwheel: false,
+  navigationControl: false,
+  mapTypeControl: false,
+  scaleControl: false,
+  draggable: true,
+  // gestureHandling: 'cooperative',
+  language: document.documentElement.getAttribute('lang') || 'en',
+  styles: mapStyle()
+}
+
+const CENTER = {
+  lat: 49.81201485236716, lng: 24.03978263623819
+}
+
+const baseFolder = window.location.href.match(/localhost|verstka/)
+  ? './assets/images/markers/'
+  : '/wp-content/themes/3d/assets/images/markers/';
+
+async function startLoadGoogleMap() {
   const script = document.createElement('script');
   let key = document.documentElement.dataset.key ? document.documentElement.dataset.key : '';
-  // if (window.location.href.match(/localhost|smarto/)) key = '';
-  // const key = '';
   script.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=initMap&language=${document.documentElement.getAttribute('lang')}&libraries=marker`;
   document.getElementsByTagName('head')[0].appendChild(script);
 }
-// setTimeout(func, 1000);
-const maps = document.querySelectorAll('.map');
-const options = {
-  rootMargin: '0px',
-  threshold: 0.1,
-};
 
-maps.forEach((image) => {
+
+const maps = document.querySelectorAll('.map');
+
+maps.forEach(($map) => {
   const callback = (entries, observer) => {
     /* Content excerpted, show below */
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        const lazyImage = entry.target;
-        lazyImage.src = lazyImage.dataset.src;
-        observer.unobserve(image);
-        func();
+        observer.unobserve($map);
+        startLoadGoogleMap();
       }
     });
   };
-  const observer = new IntersectionObserver(callback, options);
-  const target = image;
-  observer.observe(target);
+  const observer = new IntersectionObserver(callback, {
+    rootMargin: '0px',
+    threshold: 0.1,
+  });
+  observer.observe($map);
 });
 
-// eslint-disable-next-line no-unused-vars
 function initMap() {
   const gmarkers1 = [];
-  //28.4600074, 49.2384203
   const center = {
-    lat: 49.810250222382344, 
-    lng: 24.0418943600124,
+    lat: 49.81201485236716, lng: 24.03978263623819
   };
   /** Массив, куда записываются выбраные категории */
   let choosedCategories = new Set();
@@ -53,47 +63,78 @@ function initMap() {
   /** Елементы, при клике на который будет происходить фильтрация */
   const filterItems = document.querySelectorAll('[data-marker]');
   const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center,
-    scrollwheel: false,
-    navigationControl: false,
-    mapTypeControl: false,
-    scaleControl: false,
-    draggable: true,
-    // gestureHandling: 'cooperative',
-    language: document.documentElement.getAttribute('lang') || 'en',
-    styles: mapStyle()
+    ...MAP_PARAMS,
+    center: CENTER,
   });
-  window.googleMap = map;
-  const baseFolder = window.location.href.match(/localhost/) 
-    ? './assets/images/markers/'
-    : '/wp-content/themes/forest-home/assets/images/markers/';
 
-  iniCircleWithTooltip({
+  window.googleMap = map;
+  const gDistanceMarkers = [];
+
+  gDistanceMarkers.push(iniCircleWithTooltip({
     map,
     radius: 500,
     tooltipImgUrl: `${baseFolder}500m.svg`,
     center
-  })
-  iniCircleWithTooltip({
+  }))
+  gDistanceMarkers.push(iniCircleWithTooltip({
     map,
     radius: 1000,
     tooltipImgUrl: `${baseFolder}1km.svg`,
     center
-  });
+  }));
 
-  iniCircleWithTooltip({
+  gDistanceMarkers.push(iniCircleWithTooltip({
     map,
     radius: 1500,
     tooltipImgUrl: `${baseFolder}1_5km.svg`,
     center
-  });
+  }));
 
-  google.maps.event.addDomListener(window, "resize", function() {
-    console.log('resize');
-    
+  google.maps.event.addDomListener(window, "resize", function () {
     google.maps.event.trigger(map, "resize");
     map.fitBounds(bounds);
+  });
+
+  google.maps.event.addListener(map, 'zoom_changed', function () {
+    gDistanceMarkers.forEach((group) => {
+      Object.values(group).forEach((el) => {
+        if (map.zoom < 14) {
+          el.setVisible(false);
+        } else {
+          el.setVisible(true);
+        }
+      })
+    });
+
+
+    let scaledSize;
+    let anchor;
+    if (map.zoom >= 20) {
+      scaledSize = new google.maps.Size(50, 58);
+      anchor = new google.maps.Point(25, 58);
+    } else if (map.zoom >= 16) {
+      scaledSize = new google.maps.Size(30, 38);
+      anchor = new google.maps.Point(15, 38);
+    } else if (map.zoom >= 13) {
+      scaledSize = new google.maps.Size(30, 38);
+      anchor = new google.maps.Point(15, 38);
+    } else if (map.zoom >= 7) {
+      scaledSize = new google.maps.Size(20, 28);
+      anchor = new google.maps.Point(10, 28);
+    } else if (map.zoom >= 5) {
+      scaledSize = new google.maps.Size(10, 18);
+      anchor = new google.maps.Point(5, 18);
+    } else {
+      scaledSize = new google.maps.Size(5, 9);
+      anchor = new google.maps.Point(2.5, 9);
+    }
+    for (var i = 0; i < gmarkers1.length; i++) {
+      if (gmarkers1[i].category === 'main') continue;
+      var icon = gmarkers1[i].getIcon();
+      icon.scaledSize = scaledSize;
+      icon.anchor = anchor;
+      gmarkers1[i].setIcon(icon);
+    }
   });
 
 
@@ -109,7 +150,6 @@ function initMap() {
   };
 
   document.querySelectorAll('[data-marker-reset]').forEach((el) => {
-
     el.addEventListener('click', () => {
       choosedCategories = new Set();
       choosedCategories.add('main');
@@ -138,17 +178,14 @@ function initMap() {
         }
       }
       filterMarkers('main', choosedCategories);
-      console.log('choosedCategories', choosedCategories);
-      
     });
   });
 
-  
+
   const ajaxMarkers = fetchMarkersData(google);
-  
+
   ajaxMarkers.then(result => {
     putMarkersOnMap(result, map);
-    console.log('ajaxMarkers', result);
   })
 
   function putMarkersOnMap(markers, map) {
@@ -160,13 +197,10 @@ function initMap() {
     markers.forEach((marker) => {
       const category = marker.type;
 
-      // console.log('marker.icon', marker.icon);
-      
-  
       const mapMarker = new google.maps.Marker({
         map,
         category,
-        zIndex: marker.zIndex || 1,
+        zIndex: category === 'main' ? 2 : 1,
         icon: marker.icon,
         dataId: +marker.id,
         content: marker.content,
@@ -174,7 +208,7 @@ function initMap() {
       });
       mapMarker.dataId = +marker.id;
       initedMarkers.push(mapMarker);
-  
+
       google.maps.event.addListener(mapMarker, 'click', function () {
         infowindow.setContent(marker.content);
         infowindow.open(map, mapMarker);
@@ -186,28 +220,20 @@ function initMap() {
     map.initedMarkers = initedMarkers;
     markersHightlight(google, map, infowindow);
   }
-  
-  
 }
 
 
 
 function markersHightlight(google, map, infowindow) {
   const $markerLinks = document.querySelectorAll('[data-marker-id]');
-  // const infowindow = new google.maps.InfoWindow({
-  //   content: '',
-  //   maxWidth: 280,
-  // });
   querySelectorWithNodeList('[data-marker-id]', (item) => {
     item.addEventListener('click', () => {
-
       const marker = map.initedMarkers.find(el => {
         return el.dataId === +item.dataset.markerId
       });
       if (marker === undefined) return;
       infowindow.setContent(marker.content);
       infowindow.open(map, marker);
-      // console.log(marker);
     })
   })
 }
@@ -218,34 +244,15 @@ function querySelectorWithNodeList(selector, cb = () => { }) {
   $list.forEach(el => cb(el));
 }
 
-
-function markersHandler() {
-  document.querySelector('.map-wrapper')
-    .addEventListener('click', ({ target }) => {
-      const map = window.googleMap;
-      if (target.closest('[data-marker-id]') === null || !map) return;
-      const markerId = target.closest('[data-marker-id]').dataset.markerId;
-      const marker = map.initedMarkers.find(marker => marker.dataId == markerId);
-      marker && map.setCenter(marker.getPosition());
-      console.log(map.initedMarkers.find(marker => marker.dataId == markerId));
-      console.log(map);
-    })
-}
-
 function iniCircleWithTooltip({
-  map, 
-  radius, 
+  map,
+  radius,
   tooltipImgUrl,
   circleParams = {},
   tooltipParams = {},
   center
 
 }) {
-  // const center = {
-  //   lat: 49.237055962508656,
-  //   lng: 28.4270179805356,
-  // };
-
   const tooltip1MeterOffset = 0.00001374;
 
   const circle = new google.maps.Circle({
@@ -268,7 +275,7 @@ function iniCircleWithTooltip({
     map,
     icon: {
       url: tooltipImgUrl,
-      scaledSize: new google.maps.Size(56,28),
+      scaledSize: new google.maps.Size(56, 28),
     },
     ...tooltipParams
   });
@@ -280,7 +287,7 @@ function iniCircleWithTooltip({
     map,
     icon: {
       url: tooltipImgUrl,
-      scaledSize: new google.maps.Size(56,28),
+      scaledSize: new google.maps.Size(56, 28),
     },
     ...tooltipParams
   });
